8a7d6190a68590a6d774574a91a917a64d256487
sevuu <rodihasan1@gmail.com>
2021-06-06 10:09:19 +0200

    added some cool files

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,49 @@
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/01 - Brianstorm.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/02 - Teddy Picker.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/03 - D is for Dangerous.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/04 - Balaclava.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/05 - Fluorescent Adolescent.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/06 - Only Ones Who Know.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/07 - Do Me a Favour.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/08 - This House is a Circus.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/09 - If You Were There, Beware.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/10 - The Bad Thing.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/11 - Old Yellow Bricks.flac
+/home/rodi/Music/Arctic Monkeys - Favourite Worst Nightmare/12 - 505.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/01 - Tapestry of the Starless Abstract.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/02 - Xenoflux.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/03 - Of the Leper Butterflies.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/04 - Forget Not.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/05 - And Plague Flowers the Kaleidoscope.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/06 - As Icicles Fall.flac
+/home/rodi/Music/Ne Obliviscaris - Portal of I/07 - Of Petrichor Weaves Black Noise.flac
+/home/rodi/Music/Ne Obliviscaris - Citadel/01 - Painters of the Tempest - Part I (Wyrmholes).mp3
+/home/rodi/Music/Ne Obliviscaris - Citadel/02 - Painters of the Tempest - Part II (Triptych Lux).mp3
+/home/rodi/Music/Ne Obliviscaris - Citadel/03 - Painters of the Tempest - Part III (Reveries from the Stained Glass Womb).mp3
+/home/rodi/Music/Ne Obliviscaris - Citadel/04 - Pyrrhic.mp3
+/home/rodi/Music/Ne Obliviscaris - Citadel/05 - Devour Me, Colossus - Part I (Blackholes).mp3
+/home/rodi/Music/Ne Obliviscaris - Citadel/06 - Devour Me, Colossus - Part II (Contortions).mp3
+/home/rodi/Music/Ne Obliviscaris - Urn/01 - Libera, Pt. I_ Saturnine Spheres.flac
+/home/rodi/Music/Ne Obliviscaris - Urn/02 - Libera, Pt. Ii_ Ascent of Burning Moths.flac
+/home/rodi/Music/Ne Obliviscaris - Urn/03 - Intra Venus.flac
+/home/rodi/Music/Ne Obliviscaris - Urn/04 - Eyrie.flac
+/home/rodi/Music/Ne Obliviscaris - Urn/05 - Urn, Pt. I_ And Within the Void We Are Breathless.flac
+/home/rodi/Music/Ne Obliviscaris - Urn/06 - Urn, Pt. Ii_ As Embers Dance in Our Eyes.flac
+/home/rodi/Music/Opeth - Ghost Reveries/01 - Ghost of Perdition.flac
+/home/rodi/Music/Opeth - Ghost Reveries/02 - The Baying of the Hounds.flac
+/home/rodi/Music/Opeth - Ghost Reveries/03 - Beneath the Mire.flac
+/home/rodi/Music/Opeth - Ghost Reveries/04 - Atonement.flac
+/home/rodi/Music/Opeth - Ghost Reveries/05 - Reverie _ Harlequin Forest.flac
+/home/rodi/Music/Opeth - Ghost Reveries/06 - Hours of Wealth.flac
+/home/rodi/Music/Opeth - Ghost Reveries/07 - The Grand Conjuration.flac
+/home/rodi/Music/Opeth - Ghost Reveries/08 - Isolation Years.flac
+/home/rodi/Music/Royal Blood - Royal Blood/01 - Out of the Black.flac
+/home/rodi/Music/Royal Blood - Royal Blood/02 - Come on Over.flac
+/home/rodi/Music/Royal Blood - Royal Blood/03 - Figure It Out.flac
+/home/rodi/Music/Royal Blood - Royal Blood/04 - You Can Be So Cruel.flac
+/home/rodi/Music/Royal Blood - Royal Blood/05 - Blood Hands.flac
+/home/rodi/Music/Royal Blood - Royal Blood/06 - Little Monster.flac
+/home/rodi/Music/Royal Blood - Royal Blood/07 - Loose Change.flac
+/home/rodi/Music/Royal Blood - Royal Blood/08 - Careless.flac
+/home/rodi/Music/Royal Blood - Royal Blood/09 - Ten Tonne Skeleton.flac
+/home/rodi/Music/Royal Blood - Royal Blood/10 - Better Strangers.flac
