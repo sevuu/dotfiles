fd3a44078cc4f7d02183917540f1aaa661e2af4d
sevuu <rodihasan1@gmail.com>
2021-06-28 21:05:22 +0200

    chicken nugget

--- Scripts/restartpolybar.sh
+++ Scripts/restartpolybar.sh
@@ -4,6 +4,6 @@ DMENU='dmenu'
 choice=$(echo -e "restart\nkill" | $DMENU -p "Polybar")
 
 case "$choice" in
-	restart) killall polybar && polybar topbar1 &&sleep 0.3&& polybar topbar & ;;
+	restart) killall polybar && polybar topbar1 && sleep 1 && polybar topbar & ;;
 	kill) killall polybar
 esac
